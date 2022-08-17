d74f9257faf2f78f122c4151d1b812e90708903b
Öztürk,Furkan F <50831308+Furkan-rgb@users.noreply.github.com>
2022-08-17 11:11:10 +0200

    Navbar adjustments + index page setup

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,5 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Disclosure } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
-import Link from "next/link";
 import { useRouter } from "next/router";
 
 function classNames(...classes: string[]) {
@@ -23,10 +20,10 @@ export default function Navbar() {
       {({ open }) => (
         <>
           <div className="mx-auto px-2 sm:px-10 lg:px-12">
-            <div className="relative flex items-center justify-between h-16">
+            <div className="relative flex items-center justify-between h-20">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? <XIcon className="block h-6 w-6" aria-hidden="true" /> : <MenuIcon className="block h-6 w-6" aria-hidden="true" />}
                 </Disclosure.Button>
@@ -42,7 +39,7 @@ export default function Navbar() {
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <a key={item.name} href={item.href} className={classNames(item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "px-3 py-2 rounded-md text-sm font-medium")} aria-current={item.current ? "page" : undefined}>
+                      <a key={item.name} href={item.href} className={classNames(item.current ? " text-white" : "text-gray-300 bg-gradient-to-r hover:text-white ", "px-3 py-2 rounded-md text-sm font-medium")} aria-current={item.current ? "page" : undefined}>
                         {item.name}
                       </a>
                     ))}
@@ -50,43 +47,11 @@ export default function Navbar() {
                 </div>
               </div>
 
-              {/* Profile */}
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {/* Profile dropdown */}
-                <Menu as="div" className="ml-3 relative">
-                  <div>
-                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                      <span className="sr-only">Open user menu</span>
-                      <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                    </Menu.Button>
-                  </div>
-                  <Transition as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95" enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75" leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a href="#" className={classNames(active ? "bg-gray-100" : "", "block px-4 py-2 text-sm text-gray-700")}>
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a href="#" className={classNames(active ? "bg-gray-100" : "", "block px-4 py-2 text-sm text-gray-700")}>
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a href="#" className={classNames(active ? "bg-gray-100" : "", "block px-4 py-2 text-sm text-gray-700")}>
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-                <button></button>
+              {/* Connect Wallet Button */}
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 pt-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 text-sm font-medium rounded-lg group bg-gradient-to-r from-cyan-500 to-pink-500 group-hover:from-cyan-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none ">
+                  <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">Connect Wallet</span>
+                </button>
               </div>
             </div>
           </div>
@@ -94,7 +59,7 @@ export default function Navbar() {
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
-                <Disclosure.Button key={item.name} as="a" href={item.href} className={classNames(item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "block px-3 py-2 rounded-md text-base font-medium")} aria-current={item.current ? "page" : undefined}>
+                <Disclosure.Button key={item.name} as="a" href={item.href} className={classNames(item.current ? " text-white" : "text-gray-300  hover:text-white", "block px-3 py-2 rounded-md text-base font-medium")} aria-current={item.current ? "page" : undefined}>
                   {item.name}
                 </Disclosure.Button>
               ))}
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,32 @@ function HomePage() {
   return (
     <div>
       <Navbar></Navbar>
-      <h1>The home page</h1>
+      <div className="cone"></div>
+
+      <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+        <div className="sm:text-center lg:text-left">
+          <h1 className="text-4xl tracking-tight font-bold text-gray-200 sm:text-5xl sm:tracking-tight md:text-6xl md:tracking-tight">
+            <span className="block xl:inline">Decentralized world of</span> <span className="block text-gray-200 xl:inline">cryptocurrency</span>
+          </h1>
+          <p className="mt-3 text-base text-white-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 text-gray-200">Buy, sell and earn crypto.</p>
+          <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <div>
+              <button href="#" className="w-full h-full flex items-center justify-center px-8 py-3 text-base font-medium rounded-md text-white bg-gradient-to-br from-cyan-500 to-pink-500 md:py-4 md:text-lg md:px-10 hover:opacity-80 transition-all ease-in duration-75">
+                Get started
+              </button>
+            </div>
+
+            <div className="sm:mt-0 mt-2">
+              {/* Dashboard button */}
+              <div className="sm:ml-3 bg-gradient-to-r from-cyan-500 via-blue-500 to-pink-500 rounded-lg p-0.5 ">
+                <button className="w-full flex items-center justify-center text-base font-medium rounded-md px-8 py-3 md:py-3.5 md:text-lg md:px-8 bg-gray-900 hover:bg-gray-700 transition-all ease-in duration-75">
+                  <a href="/dashboard">View Dashboard</a>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
