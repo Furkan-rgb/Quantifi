956585e9b4732ddffa6e2d22f023769312a05e73
Öztürk,Furkan F <50831308+Furkan-rgb@users.noreply.github.com>
2022-09-03 13:33:24 +0200

    Docs added to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -67,6 +67,28 @@ export default function Navbar() {
                         {item.name}
                       </a>
                     ))}
+                    <a
+                      href="Https://joel-lowe.gitbook.io/quantifi"
+                      className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md bg-gradient-to-r hover:text-white"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      Docs
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="inline-block w-4 h-4 pl-1"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                        />
+                      </svg>
+                    </a>
                   </div>
                 </div>
               </div>
--- pages/my-page.tsx
+++ pages/my-page.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import myPageAbi from "../components/abi/QIT.json";
-import erc20ABI from "../components/abi/erc20.json"
+import erc20ABI from "../components/abi/erc20.json";
 import { useWeb3React } from "@web3-react/core";
 import { BigNumber, ethers } from "ethers";
 
@@ -8,26 +8,23 @@ function MyPage() {
   const [currentTab, setCurrentTab] = useState<string>("deposit");
   const [inputValue, setInputValue] = useState<string>("0");
   const [outputValue, setOutputValue] = useState<string>();
+  const [holdingValue, setHoldingValue] = useState<string>();
   const fromValue = useRef(0);
   const [contractInfo, setContractInfo] = useState<{
     address: ethers.Contract["address"];
     tokenName: string;
     qitbalance: ethers.BigNumber;
-    allowance:ethers.BigNumber;
+    allowance: ethers.BigNumber;
   }>({
     address: "-",
     tokenName: "QIT",
     qitbalance: BigNumber.from(0),
-    allowance:BigNumber.from(0),
+    allowance: BigNumber.from(0),
   });
   const minDeposit = 1000; // this will be updated to actual value in initiateContract()
 
   const { library, chainId, account, active } = useWeb3React();
-  const QIT = new ethers.Contract(
-    "0x4C4470D0B9c0dD92B25Be1D2fB5181cdA7e6E3f7",
-    myPageAbi,
-    library
-  );
+  const QIT = new ethers.Contract("0x4C4470D0B9c0dD92B25Be1D2fB5181cdA7e6E3f7", myPageAbi, library);
   const ERC20 = new ethers.Contract(
     "0xEcAD8721BA48dBdc0eac431D68A0b140F07c0801",
     erc20ABI,
@@ -37,56 +34,80 @@ function MyPage() {
     return classes.filter(Boolean).join(" ");
   }
 
+  async function getHoldingValue(_address: string) {
+    const _holdingValue = await QIT.getHoldingValue(_address);
+    setHoldingValue(ethers.utils.formatUnits(_holdingValue, 6));
+  }
+
   async function getDepositValue(value: string) {
     const number = parseInt(value, 10);
     if (number >= minDeposit) {
-      var n = ethers.utils.parseEther(value);
-      const deposit = await QIT.getDepositReturn(ethers.utils.parseEther(value));
+      const n = ethers.utils.parseEther(value);
+      const deposit = await QIT.getDepositReturn(n);
       setOutputValue(ethers.utils.formatUnits(deposit, 6));
-      } else {
-        //TODO: Change to red and show Min Deposit = $x
-      }     
+    } else {
+      //TODO: Change to red and show Min Deposit = $x
+    }
   }
 
   async function swapOrApprove() {
-    if(currentTab=='deposit'){
-      if(contractInfo.allowance<ethers.utils.parseEther(inputValue)){
+    if (currentTab == "deposit") {
+      if (contractInfo.allowance < ethers.utils.parseEther(inputValue)) {
         const ERC20connect = ERC20.connect(library.getSigner());
-        await ERC20connect.approve(QIT.address,ethers.utils.parseEther(inputValue),{gasLimit:100000});
+        await ERC20connect.approve(QIT.address, ethers.utils.parseEther(inputValue), {
+          gasLimit: 100000,
+        });
         // update after completion
       } else {
-        console.log("Execute Swap")
+        const QITconnect = QIT.connect(library.getSigner());
+        await QITconnect.depositToFund(ethers.utils.parseEther(inputValue));
+        console.log("Execute Swap");
       }
     } else {
       console.log("Execute Withdrawal");
     }
-
   }
 
   async function initiateContract() {
     setContractInfo({
       address: QIT.address,
-      tokenName: QIT.tokenName,
+      tokenName: await QIT.name(),
       qitbalance: await QIT.balanceOf(account),
-      allowance: await ERC20.allowance(account,QIT.address),
+      allowance: await ERC20.allowance(account, QIT.address),
     });
+    if (account != null || account !== undefined) {
+      await getHoldingValue(account!);
+      console.log(account);
+    } else {
+      console.log("No account");
+    }
   }
 
-  async function basicUpdate(){
+  function swapOrApprovalButton() {}
+
+  // Function to update details
+  async function basicUpdate() {
     setContractInfo({
       address: contractInfo.address,
       tokenName: contractInfo.tokenName,
       qitbalance: await QIT.balanceOf(account),
-      allowance: await ERC20.allowance(account,QIT.address),
+      allowance: await ERC20.allowance(account, QIT.address),
     });
+    if (account != null || account !== undefined) {
+      getHoldingValue(account!);
+    } else {
+      console.log("No account");
+    }
   }
 
+  // If there is a provider, but no account then initiateContract(). Keeps track of 'active' value changes
   useEffect(() => {
     if (active && contractInfo.address === "-") {
       initiateContract();
     }
   }, [active]);
 
+  // Monitor and log transactions
   useEffect(() => {
     if (contractInfo.address !== "-") {
       QIT.on("Transfer", (from, to, amount, event) => {
@@ -94,6 +115,16 @@ function MyPage() {
       });
     }
   }, [contractInfo.address]);
+
+  // Trigger when qit balance changes
+  useEffect(() => {
+    async function update() {
+      await basicUpdate();
+    }
+    if (contractInfo.address !== "-") {
+      update();
+    }
+  }, [contractInfo.qitbalance]);
 
   return (
     <>
@@ -115,14 +146,16 @@ function MyPage() {
                 <span className="block py-1 mb-2 mr-2 text-base font-semibold text-gray-700 rounded-full">
                   Tokens
                 </span>
-                <span className="text-right">{ethers.utils.formatUnits(contractInfo.qitbalance,6)} QIT</span>
+                <span className="text-right">
+                  {ethers.utils.formatUnits(contractInfo.qitbalance, 6)} QIT
+                </span>
               </div>
 
               <div className="flex justify-between">
                 <span className="block py-1 mb-2 mr-2 text-base font-semibold text-gray-700 rounded-full">
                   Value
                 </span>
-                <span className="text-right">USDT</span>
+                <span className="text-right">{holdingValue} USDT</span>
               </div>
 
               <div className="flex justify-between">
@@ -202,14 +235,14 @@ function MyPage() {
                       setInputValue(e.target.value), getDepositValue(e.target.value);
                     }}
                     type="number"
-                    name="floating_email"
-                    id="floating_email"
+                    name="floating_input"
+                    id="floating_input"
                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" "
                     required
                   />
                   <label
-                    htmlFor="floating_email"
+                    htmlFor="floating_input"
                     className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
                     From
@@ -221,29 +254,35 @@ function MyPage() {
                 <div className="relative z-0 flex w-full mb-6 group">
                   <input
                     type="number"
-                    name="floating_password"
-                    id="floating_password"
+                    name="floating_output"
+                    id="floating_output"
                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" "
                     disabled
                   />
                   <label
-                    htmlFor="floating_password"
+                    htmlFor="floating_output"
                     className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
-                   To {outputValue}
+                    To {outputValue}
                   </label>
                   <span className="inline-flex items-center px-3 text-sm text-gray-900 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer">
-                    {currentTab == "deposit" ? contractInfo?.tokenName : "USDT"}
+                    {currentTab == "deposit" ? "QIT" : "USDT"}
                   </span>
                 </div>
 
                 <button
-                  onClick={()=>{swapOrApprove()}}
+                  onClick={() => {
+                    swapOrApprove();
+                  }}
                   type="button"
                   className="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
                 >
-                  {currentTab=="deposit"?(contractInfo.allowance<ethers.utils.parseEther(inputValue)?"Give permission to deposit USDT":"Execute Swap"):"Execute Swap"}
+                  {currentTab == "deposit"
+                    ? contractInfo.allowance < ethers.utils.parseEther(inputValue)
+                      ? "Give permission to deposit USDT"
+                      : "Execute Swap"
+                    : "Execute Swap"}
                 </button>
               </form>
             </div>
