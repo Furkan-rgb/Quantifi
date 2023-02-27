e80cf7b4bed0bdb5c74b83cefeabbdd8059b5799
Öztürk,Furkan F <50831308+Furkan-rgb@users.noreply.github.com>
2023-02-27 09:22:39 +0100

    bg update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import { IoMdSettings } from "react-icons/io";
 
 function Footer() {
   return (
-    <footer className="p-4 mt-auto bg-transparent bg-black border-t border-indigo-500 shadow md:flex md:items-center md:justify-between md:p-6">
-      <ul className="flex flex-wrap items-center justify-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+    <footer className="mt-auto bg-transparent p-4 shadow md:flex md:items-center md:justify-between md:p-6">
+      <ul className="mt-3 flex flex-wrap items-center justify-center text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
           <a href="#" className="ml-4 hover:underline md:mr-6 ">
             Contact
@@ -24,7 +24,7 @@ function Footer() {
           </a>
         </li>
       </ul>
-      <div className="flex justify-center mt-4 space-x-6 md:mt-0">
+      <div className="mt-4 flex justify-center space-x-6 md:mt-0">
         <a
           href="https://discord.com/"
           className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
